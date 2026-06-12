783ca4ab8afcad26d6e4dfb67339d5731b51b61a
Mahessh01 <Mahesshofficial@gmail.com>
2026-06-12 23:13:49 +0545

    carts file

--- carts/views.py
+++ carts/views.py
@@ -47,15 +47,25 @@ def add_cart(request, product_id):
 
     return redirect('cart')
 
-def remove_cart(request,product_id):
-    cart= cart.objects.get(cart_id= _cart_id(request))
-    product= get_object_or_404(Product, id= product_id)
-    cart_item= CartItem.objects.get(product= product,cart=cart)
-    if cart_item.quantity > 1:
-        cart_item.quantity -= 1
-        cart_item.save()
-    else:
-        cart_item.delete()
+def remove_cart(request, product_id):
+    product = Product.objects.get(id=product_id)
+
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_item = CartItem.objects.get(
+            product=product,
+            cart=cart
+        )
+
+        if cart_item.quantity > 1:
+            cart_item.quantity -= 1
+            cart_item.save()
+        else:
+            cart_item.delete()
+
+    except:
+        pass
+
     return redirect('cart')
         
 

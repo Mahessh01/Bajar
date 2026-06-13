89a1b4c0b7f55489dd316b283dc6d0852c4199d8
Mahessh01 <Mahesshofficial@gmail.com>
2026-06-13 23:23:33 +0545

    updated file

--- carts/views.py
+++ carts/views.py
@@ -15,6 +15,8 @@ def _cart_id(request):
 
 
 def add_cart(request, product_id):
+    color= request.GET('color')
+    size= request.GET('size')
     product = Product.objects.get(id=product_id)
 
     try:

9a6422027c5dcb9b027b6ac5f98ee28a0df3d988
Mahessh01 <Mahesshofficial@gmail.com>
2026-06-20 23:36:13 +0545

    modified file

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,5 @@
+from itertools import product
+
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from accounts.forms import RegisterationForm
@@ -13,6 +15,8 @@ from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.encoding import force_bytes, force_str
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import EmailMessage
+from carts.models import Cart, CartItem
+from carts.views import _cart_id
 
 
 # =========================
@@ -95,6 +99,19 @@ def login(request):
         user = authenticate(request, username=email, password=password)
 
         if user is not None:
+            try:
+                cart= Cart.objects.get(cart_id=_cart_id(request))
+                is_cart_item_exists= CartItem.objects.filter(product=product, cart=cart).exists()
+                if is_cart_item_exists:
+                    cart_item= CartItem.objects.filter(cart=cart)
+                    
+                    for item in cart_item:
+                        item.user= user
+                        item.save()
+                
+                
+            except:
+                pass
             auth_login(request, user)
             messages.success(request, "Logged in successfully!")
             return redirect('store')

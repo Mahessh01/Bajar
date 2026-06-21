4022968e1879512e050b518155934140230f5221
Mahessh01 <Mahesshofficial@gmail.com>
2026-06-21 23:50:14 +0545

    some changes done

--- carts/urls.py
+++ carts/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .import views
+from . import views
 
 urlpatterns = [
-    path('',views.cart,name='cart'),
-    path('add_cart/<int:product_id>/',views.add_cart,name= 'add_cart'),
-    path('remove_cart/<int:product_id>/',views.remove_cart,name= 'remove_cart'),
-    path('checkout/',views.checkout, name="checkout"),
-    
+    path('', views.cart, name='cart'),
+    path('add_cart/<int:product_id>/', views.add_cart, name='add_cart'),
+
+    path('increase_cart/<int:cart_item_id>/', views.increase_cart, name='increase_cart'),
+    path('decrease_cart/<int:cart_item_id>/', views.decrease_cart, name='decrease_cart'),
+    path('remove_cart_item/<int:cart_item_id>/', views.remove_cart_item, name='remove_cart_item'),
+
+    path('checkout/', views.checkout, name='checkout'),
 ]
